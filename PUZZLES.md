e832a0c27134a7cfe65d08f801df9e6628251f5a
Jan Hovancik <conta.srdr@gmail.com>
2016-01-04 10:13:51 +0100

    init fork from yegor256/jekyll-github-deploy

---
+++ PUZZLES.md
@@ -0,0 +1,4 @@
+## Puzzles
+
+There are 0 active puzzles (0 total):
+
---
+++ bash/deploy.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+set -x
+set -e
+set -o pipefail
+
+URL=$1
+BRANCH=$2
+SRC=$(pwd)
+TEMP=$(mktemp -d -t jgd-XXX)
+trap "rm -rf ${TEMP}" EXIT
+CLONE=${TEMP}/clone
+COPY=${TEMP}/copy
+
+echo -e "Cloning Github repository:"
+git clone "${URL}" "${CLONE}"
+cp -R ${CLONE} ${COPY}
+
+cd "${CLONE}"
+
+echo -e "\nBuilding Jekyll site:"
+rm -rf _site
+
+if [ -r _config-deploy.yml ]; then
+  jekyll build --config _config.yml,_config-deploy.yml
+else
+  jekyll build
+fi
+
+if [ ! -e _site ]; then
+  echo -e "\nJekyll didn't generate anything in _site!"
+  exit -1
+fi
+
+cp -R _site ${TEMP}
+
+cd ${TEMP}
+rm -rf ${CLONE}
+mv ${COPY} ${CLONE}
+cd ${CLONE}
+
+echo -e "\nPreparing ${BRANCH} branch:"
+if [ -z "$(git branch -a | grep origin/${BRANCH})" ]; then
+  git checkout --orphan "${BRANCH}"
+else
+  git checkout "${BRANCH}"
+fi
+
+echo -e "\nDeploying into ${BRANCH} branch:"
+rm -rf *
+cp -R ${TEMP}/_site/* .
+rm -f README.md
+git add .
+git commit -am "new version $(date)" --allow-empty
+git push origin ${BRANCH} 2>&1 | sed 's|'$URL'|[skipped]|g'
+
+echo -e "\nCleaning up:"
+rm -rf "${CLONE}"
+rm -rf "${SITE}"
